c2affb7c660420dabf3fc4765e962f31fa2c17df
taisa-asiato <taisa.aikou@gmail.com>
2020-01-31 00:51:24 +0900

    [MOD] varcount function implemented

--- 11/SymbolTable.c
+++ 11/SymbolTable.c
@@ -1,6 +1,5 @@
 #include "define.h"
 
-
 void init_SymbolTable() {
 	cls = malloc( sizoef( scope_t ) );
 	strcpy( cls->name, "" );
@@ -48,4 +47,58 @@ void my_define( int iscls, char * symbol_name, char * type, char * proper, int n
 		tmp->next = NULL;
 		subp = tmp;
 	}
+}
+
+scope_t * list_Find_Scope_Cls( char * symbol_name ) {
+	scope_t * tmp = cls;
+	while ( tmp != NULL ) {
+		if ( strcmp( symbol_name, tmp->name ) == 0 ) {
+			return tmp;
+		}
+		tmp = tmp->next;
+	}
+	return NULL;
+}
+
+scope_t * list_Find_Scope_Sub( char * symbol_name ) {
+	scope_t * tmp = subp;
+	while ( tmp != NULL ) {
+		if ( strcmp( symbol_name, tmp->name ) ) {
+			return tmp;
+		}
+		tmp = tmp->next;
+	}
+	return NULL
+}
+
+scope_t * list_Find_Scope( char * symbol_name ) {
+	scope_t * tmp;
+
+	if ( tmp = list_Find_Scope_Cls( symbol_name ) ) {
+		return tmp;
+	} else {
+		return list_Find_Scope_Sub( symbol_name );
+	}
+}
+
+int var_Count( char * typeof ) {
+	int cnt = 0;
+	if ( strcmp( typeof, "static" ) == 0 || strcmp( typeof, "field" ) == 0 ) {
+		scope_t * tmp = cls->next; 
+		while ( tmp ) {
+			if ( strcmp( tmp->type, typeof ) == 0 ) {
+				cnt += 1;
+			}
+			tmp = tmp->next;
+		}
+	} else if ( strcmp( typeof, "var" ) == 0 || strcmp( typeof, "argument" ) == 0 ) {
+		scope_t * tmp = sub->next;
+		while ( tmp ) {
+			if ( strcmp( tmp->type, typeof ) == 0 ) {
+				cnt += 1;
+			}　
+			tmp = tmp->next;
+		}
+	}
+	return cnt;
 }
